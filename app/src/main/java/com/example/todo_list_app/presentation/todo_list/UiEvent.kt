69ec6d55ad77415acab8b00ec1ffd1228be746f1
furkan harmancı <rocklee34@hotmail.com>
2023-11-08 15:32:42 +0300

    feat: events added :sparkles:

---
+++ app/src/main/java/com/example/todo_list_app/presentation/todo_list/UiEvent.kt
@@ -0,0 +1,10 @@
+package com.example.todo_list_app.presentation.todo_list
+
+sealed class UiEvent {
+    data object PopBackStack : UiEvent()
+    data class Navigate(val route: String) : UiEvent()
+    data class ShowSnackBar(
+        val message: String,
+        val action: String? = null
+    ): UiEvent()
+}
